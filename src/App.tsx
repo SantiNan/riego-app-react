252a71d05e72876f1de56a62b911e577e47c08f0
SantiNan <58916751+SantiNan@users.noreply.github.com>
2026-03-14 09:10:33 -0300

    Fix env

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
       <GlobalStyles />
       <MQTTProvider>
         <ToastProvider>
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
               <Route path="/"              element={<Home />} />
               <Route path="/manual"        element={<Manual />} />
